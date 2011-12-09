5dfdc1eebfa963663b1f3074cb6f39c310152331
Myztiq <ryan@ryan-kahn.com>
2011-12-09 00:28:20 -0500

    Updated scripts

--- scripts/deckjs/extensions/status/deck.status.js
+++ scripts/deckjs/extensions/status/deck.status.js
@@ -1,10 +1,3 @@
-/*!
-Deck JS - deck.status
-Copyright (c) 2011 Caleb Troughton
-Dual licensed under the MIT license and GPL license.
-https://github.com/imakewebthings/deck.js/blob/master/MIT-license.txt
-https://github.com/imakewebthings/deck.js/blob/master/GPL-license.txt
-*/
 
 /*
 This module adds a (current)/(total) style status indicator to the deck.
